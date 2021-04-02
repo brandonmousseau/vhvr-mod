53355bc87e7f2c4f5b67110ee730840c9059ad89
Brandon Mousseau <brandon.a.mousseau@gmail.com>
2021-04-02 13:03:25 -0700

    Workaround to avoid incompatibility
    This changes the Valve OpenVR plugin to always return true for the
    method used to determine whether or not to use steamvr input. This is
    necessary because I found that there is at least one incompatibility
    with another BepInEx plugin name ConfigurationManager. It isn't clear
    what the root cause is, but calling GetTypes, as is done in
    OpenVRHelpers.cs, causes an exception to be thrown when this plugin is
    loaded first. Since for this mod we want this to always be true, I'll
    just hard code it instead to avoid the exception. It would be best to
    understand the root cause of why the error is thrown, but this seems to
    at least prevent the crash.

---
+++ Unity/ValheimVR/Library/PackageCache/com.valvesoftware.unity.openvr@77512074b981-1617322698727/Runtime/DeviceLayouts.cs
@@ -0,0 +1,11 @@
+﻿#if ENABLE_VR && UNITY_INPUT_SYSTEM && !PACKAGE_DOCS_GENERATION
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.Scripting;
+
+namespace Unity.XR.OpenVR
+{
+}
+#endif
