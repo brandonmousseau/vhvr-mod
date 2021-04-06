a95d24e8c95775879d00cab4aa91d4066a631042
Brandon Mousseau <brandon.a.mousseau@gmail.com>
2021-04-05 21:37:55 -0700

    Dont pause or change render scale on dashboard
    This patch skips a SteamVR_Render function that is called when the
    dashboard gains or loses focus. The function is responsible for pausing
    the game when the dashboard is visible as well as setting render scale
    to 0.5. Pausing the game breaks the game completely, so when the game is
    unpaused, things do not get restored.
    
    Setting the renderscale to half doesn't break the game completely, but
    things still end up being blurry even after the render scale is
    restored, so something doesn't work right there.
    
    Skipping it doesn't cause any problems and fixes a major problem users
    have.
    
    fixes #4

--- ValheimVRMod/Patches/Misc.cs
+++ ValheimVRMod/Patches/Misc.cs
@@ -1,11 +1,12 @@
 ﻿using Unity.XR.OpenVR;
 using HarmonyLib;
+using Valve.VR;
 
 namespace ValheimVRMod.Patches
 {
 
     // This patch just forces IsUsingSteamVRInput to always return true.
-    // Without this, there seems to be some problem with how the DLL namespaces
+    // Without this, there seems to be some problem with how the DLL namespacest
     // are loaded when using certain other mods. Normally this method will result
     // in a call to the Assembly.GetTypes method, but for whatever reason, this
     // ends up throwing an exception and crashes the mod. The mod I know that causes
@@ -17,6 +18,22 @@ namespace ValheimVRMod.Patches
         public static bool Prefix(ref bool __result)
         {
             __result = true;
+            return false;
+        }
+    }
+
+    // This method is responsible for pausing the game when the
+    // steam dashboard is shown as well as sets render scale
+    // to half while the dashboard is showing. Both of these
+    // break the game - pausing ends up freezing most of the
+    // game functions and the render scale doesn't get restored
+    // properly. So this patch skips the function. Dashboard still
+    // is functional.
+    [HarmonyPatch(typeof(SteamVR_Render), "OnInputFocus")]
+    class SteamVR_Render_OnInputFocus_Patch
+    {
+        public static bool Prefix()
+        {
             return false;
         }
     }
