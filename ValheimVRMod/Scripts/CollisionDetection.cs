1b6344b40ca9fd8c0f95d43f09c18a9bc30ca614
Andi1986 <andreas.hof@gmx.de>
2021-05-01 19:31:15 +0200

    lumberjacking works kinda

--- ValheimVRMod/Scripts/CollisionDetection.cs
+++ ValheimVRMod/Scripts/CollisionDetection.cs
@@ -1,14 +1,26 @@
+using System.Collections.Generic;
 using static ValheimVRMod.Utilities.LogUtils;
 using System.ComponentModel;
+using HarmonyLib;
 using UnityEngine;
 using ValheimVRMod.Utilities;
+using ValheimVRMod.VRCore;
 
 namespace ValheimVRMod.Scripts
 {
     public class CollisionDetection : MonoBehaviour
     {
         
+        private const float MIN_DISTANCE = 0.75f;
+        private const int MAX_SNAPSHOTS = 10;    
+        
+        private bool scriptActive = false;
         private GameObject colliderParent = new GameObject();
+        private int tickCounter = 0;
+        private List<Vector3> snapshots = new List<Vector3>();
+
+        public Vector3 lastHitPoint;
+        public Collider lastHitCollider;
 
         private void OnTriggerEnter(Collider collider)
         {
@@ -25,28 +37,33 @@ namespace ValheimVRMod.Scripts
                 return;
             }
 
-            Debug.Log("ITEM SHARED NAME: " + Player.m_localPlayer.GetRightItem().m_shared.m_name);
-            Debug.Log("Collision Detected. OWN: " + gameObject.layer + " - " + name +
-                      " OTHER: " + collider.gameObject.layer + " - " + collider.gameObject.name);
-
-            
-            IDestructible destructible = collider.transform.GetComponentInParent<IDestructible>();
-
-            if (destructible == null)
+            if (hasMomentum())
             {
-                Debug.Log("NO DESTRUCTIBLE FOUND");
+                Debug.Log("YES, HAS MOMEMNTUM");
+                lastHitPoint = transform.position;
+                lastHitCollider = collider;
+
+                Attack attack = Player.m_localPlayer.GetRightItem().m_shared.m_attack.Clone();
+                attack.Start(Player.m_localPlayer, null,null,
+                    AccessTools.FieldRefAccess<Player, CharacterAnimEvent>(Player.m_localPlayer, "m_animEvent"), 
+                    null,  Player.m_localPlayer.GetRightItem(), null, 0.0f, 0.0f);
+                
             }
             else
             {
-                Debug.Log("DESTRUCTIBLE FOUND: " + typeof(Destructible));
+                Debug.Log("NOPE, HAS NO MOMEMNTUM");
             }
+            
+            Debug.Log("ITEM SHARED NAME: " + Player.m_localPlayer.GetRightItem().m_shared.m_name);
+            Debug.Log("Collision Detected. OWN: " + gameObject.layer + " - " + name +
+                      " OTHER: " + collider.gameObject.layer + " - " + collider.gameObject.name);
 
         }
 
         private void OnRenderObject()
         {
 
-            if (!GetComponent<MeshRenderer>().enabled)
+            if (!scriptActive)
             {
                 return;
             }
@@ -69,13 +86,60 @@ namespace ValheimVRMod.Scripts
                 colliderParent.transform.localPosition = colliderData.pos;
                 colliderParent.transform.localRotation = Quaternion.Euler(colliderData.euler);
                 colliderParent.transform.localScale = colliderData.scale;
-                GetComponent<MeshRenderer>().enabled = true;
+                setScriptActive(true);
+
             }
             catch (InvalidEnumArgumentException)
             {
-                GetComponent<MeshRenderer>().enabled = false;
+                setScriptActive(false);
                 LogDebug("Invalid Weapon Data for: " + name);
             }
+        }
+
+        public void setScriptActive(bool active)
+        {
+            scriptActive = active;
+            
+            if (!active)
+            {
+                snapshots.Clear();
+            }
+        }
+        
+        private void FixedUpdate() 
+        {
+
+            if (!scriptActive)
+            {
+                return;
+            }
+            
+            tickCounter++;
+            if (tickCounter < 10)
+            {
+                return;
+            }
+
+            snapshots.Add(transform.position);
+        
+            if (snapshots.Count > MAX_SNAPSHOTS)
+            {
+                snapshots.RemoveAt(0);   
+            }
+            tickCounter = 0;
+        }
+        
+        public bool hasMomentum()
+        {
+            foreach (Vector3 snapshot in snapshots)
+            {
+                if (Vector3.Distance(snapshot, transform.position) > MIN_DISTANCE)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
         
     }
