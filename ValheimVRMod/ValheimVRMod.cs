a1f1d555283f81cd89faff580b04b6692cba701b
Brandon Mousseau <brandon.a.mousseau@gmail.com>
2021-07-02 21:04:32 -0700

    Update version number

--- ValheimVRMod/ValheimVRMod.cs
+++ ValheimVRMod/ValheimVRMod.cs
@@ -9,7 +9,7 @@ using static ValheimVRMod.Utilities.LogUtils;
 
 namespace ValheimVRMod
 {
-    [BepInPlugin("org.bepinex.plugins.valheimvrmod", "ValheimVR Mod", "0.5.3")]
+    [BepInPlugin("org.bepinex.plugins.valheimvrmod", "ValheimVR Mod", "0.6.0")]
     public class ValheimVRMod : BaseUnityPlugin
     {
 
