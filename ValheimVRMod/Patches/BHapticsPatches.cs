3fa18f523815c5f176c5d6d0ef599cd0c4775d00
Astienth <astienth@gmail.com>
2022-02-12 17:25:51 +0100

    bhaptics patches class for hooks not in vrmod

---
+++ ValheimVRMod/Patches/BHapticsPatches.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using BhapticsTactsuit;
+
+namespace ValheimVRMod.Patches
+{
+    [HarmonyPatch(typeof(Player), "EatFood")]
+    class Player_EatingFood_Patch
+    {
+        public static void Postfix(Player __instance)
+        {
+            if (__instance != Player.m_localPlayer || TactsuitVR.Instance.suitDisabled)
+            {
+                return;
+            }
+            TactsuitVR.Instance.PlaybackHaptics("Eating");
+        }
+    }
+}
