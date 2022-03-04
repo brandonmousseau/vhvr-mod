898dcbfdfcd21fef0ce1e36e1e48bebba2112135
Aceship <terrenceleroy@yahoo.com>
2022-03-04 09:21:18 +0700

    Changed some if to early return

--- ValheimVRMod/Scripts/WeaponWield.cs
+++ ValheimVRMod/Scripts/WeaponWield.cs
@@ -80,85 +80,100 @@ namespace ValheimVRMod.Scripts {
         }
         private void KnifeWield()
         {
-            if (_isTwoHanded == isTwoHanded.SingleHanded)
+            if (_isTwoHanded != isTwoHanded.SingleHanded)
             {
-                if (SteamVR_Actions.valheim_Grab.GetState(mainHandInputSource))
-                {
-                    ResetOffset();
-                    transform.localRotation = transform.localRotation * Quaternion.AngleAxis(180, Vector3.right);
-                    weaponForward = transform.forward;
-                    weaponSubPos = true;
-                }else if (weaponSubPos)
-                {
-                    ResetOffset();
-                    weaponSubPos = false;
-                }
+                return;
             }
-            
+
+            if (SteamVR_Actions.valheim_Grab.GetState(mainHandInputSource))
+            {
+                ResetOffset();
+                transform.localRotation = transform.localRotation * Quaternion.AngleAxis(180, Vector3.right);
+                weaponForward = transform.forward;
+                weaponSubPos = true;
+            }
+            else if (weaponSubPos)
+            {
+                ResetOffset();
+                weaponSubPos = false;
+            }
+
         }
         private void UpdateTwoHandedWield()
         {
-            if (VHVRConfig.TwoHandedWield()) {
-                if (SteamVR_Actions.valheim_Grab.GetState(SteamVR_Input_Sources.LeftHand) && SteamVR_Actions.valheim_Grab.GetState(SteamVR_Input_Sources.RightHand)) {
-                    var mainHand = VRPlayer.rightHand;
-                    var offHand = VRPlayer.leftHand;
-                    float handAngleDiff = Vector3.Dot(GetHandWieldDirection(), VRPlayer.rightHand.transform.InverseTransformPoint(VRPlayer.leftHand.transform.position).normalized);
-                    if (_isTwoHanded == isTwoHanded.SingleHanded) {
-                        if (handAngleDiff > 0.6f) {
-                            _isTwoHanded = isTwoHanded.MainRight;
-                            if (isSpear()) {
-                                _isTwoHanded = isTwoHanded.MainLeft;
-                            }
-                        }
-                        else if (handAngleDiff < -0.6f) {
-                            _isTwoHanded = isTwoHanded.MainLeft;
-                            if (isSpear()) {
-                                _isTwoHanded = isTwoHanded.MainRight;
-                            }
-                        }
-                        else {
-                            return;
-                        }
-                    }
-                    if (_isTwoHanded == isTwoHanded.MainLeft) {
-                        mainHand = VRPlayer.leftHand;
-                        offHand = VRPlayer.rightHand;
-                    }
-                    var offsetPos = Vector3.Distance(mainHand.transform.position, rotSave.transform.position);
-                    var handDist = Vector3.Distance(mainHand.transform.position, offHand.transform.position);
-                    var inversePosition = mainHand.transform.position - offHand.transform.position;
-                    var distLimit = 0f;
-                    var distMultiplier = 0f;
-                    var originMultiplier = -0.1f;
-                    var rotOffset = 180;
-                    var handForward = new Vector3(0, -0.45f, 0.55f);
-                    var handAvgVector = ((offHand.transform.TransformDirection(handForward) + mainHand.transform.TransformDirection(handForward)) / 2).normalized;
-                    switch (attack.m_attackAnimation) {
-                        case "spear_poke":
-                            distMultiplier = 0.1f;
-                            distLimit = 0.1f;
-                            originMultiplier = 0.2f;
-                            break;
-                    }
-                    var CalculateDistance = (inversePosition.normalized * distMultiplier / Mathf.Max(handDist, distLimit)) - inversePosition.normalized * originMultiplier;
-                    ResetOffset();
-                    transform.position = mainHand.transform.position + (CalculateDistance);
-                    transform.LookAt(mainHand.transform.position + inversePosition.normalized * 5, transform.up);
-                    transform.localRotation = transform.localRotation * (rotSave.transform.localRotation) * Quaternion.AngleAxis(180, Vector3.right) * Quaternion.AngleAxis(rotOffset, transform.InverseTransformDirection(inversePosition));
+            if (!VHVRConfig.TwoHandedWield()) {
+                return;
+            }
 
-                    //Atgeir Rotation fix
-                    if (attack.m_attackAnimation == "atgeir_attack")
+            if (SteamVR_Actions.valheim_Grab.GetState(SteamVR_Input_Sources.LeftHand) && SteamVR_Actions.valheim_Grab.GetState(SteamVR_Input_Sources.RightHand))
+            {
+                var mainHand = VRPlayer.rightHand;
+                var offHand = VRPlayer.leftHand;
+                float handAngleDiff = Vector3.Dot(GetHandWieldDirection(), VRPlayer.rightHand.transform.InverseTransformPoint(VRPlayer.leftHand.transform.position).normalized);
+                if (_isTwoHanded == isTwoHanded.SingleHanded)
+                {
+                    if (handAngleDiff > 0.6f)
                     {
-                        transform.localRotation = (transform.localRotation * Quaternion.AngleAxis(-20, Vector3.up)) * Quaternion.AngleAxis(-5, Vector3.right);
+                        _isTwoHanded = isTwoHanded.MainRight;
+                        if (isSpear())
+                        {
+                            _isTwoHanded = isTwoHanded.MainLeft;
+                        }
                     }
+                    else if (handAngleDiff < -0.6f)
+                    {
+                        _isTwoHanded = isTwoHanded.MainLeft;
+                        if (isSpear())
+                        {
+                            _isTwoHanded = isTwoHanded.MainRight;
+                        }
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                if (_isTwoHanded == isTwoHanded.MainLeft)
+                {
+                    mainHand = VRPlayer.leftHand;
+                    offHand = VRPlayer.rightHand;
+                }
+                var offsetPos = Vector3.Distance(mainHand.transform.position, rotSave.transform.position);
+                var handDist = Vector3.Distance(mainHand.transform.position, offHand.transform.position);
+                var inversePosition = mainHand.transform.position - offHand.transform.position;
+                var distLimit = 0f;
+                var distMultiplier = 0f;
+                var originMultiplier = -0.1f;
+                var rotOffset = 180;
+                var handForward = new Vector3(0, -0.45f, 0.55f);
+                var handAvgVector = ((offHand.transform.TransformDirection(handForward) + mainHand.transform.TransformDirection(handForward)) / 2).normalized;
+                switch (attack.m_attackAnimation)
+                {
+                    case "spear_poke":
+                        distMultiplier = 0.1f;
+                        distLimit = 0.1f;
+                        originMultiplier = 0.2f;
+                        break;
+                }
+                var CalculateDistance = (inversePosition.normalized * distMultiplier / Mathf.Max(handDist, distLimit)) - inversePosition.normalized * originMultiplier;
+                ResetOffset();
+                transform.position = mainHand.transform.position + (CalculateDistance);
+                transform.LookAt(mainHand.transform.position + inversePosition.normalized * 5, transform.up);
+                transform.localRotation = transform.localRotation * (rotSave.transform.localRotation) * Quaternion.AngleAxis(180, Vector3.right) * Quaternion.AngleAxis(rotOffset, transform.InverseTransformDirection(inversePosition));
 
-                    weaponForward = transform.forward;
-                    return;
+                //Atgeir Rotation fix
+                if (attack.m_attackAnimation == "atgeir_attack")
+                {
+                    transform.localRotation = (transform.localRotation * Quaternion.AngleAxis(-20, Vector3.up)) * Quaternion.AngleAxis(-5, Vector3.right);
                 }
-                else if (SteamVR_Actions.valheim_Grab.GetStateUp(SteamVR_Input_Sources.LeftHand) || SteamVR_Actions.valheim_Grab.GetStateUp(SteamVR_Input_Sources.RightHand)) {
-                    _isTwoHanded = isTwoHanded.SingleHanded;
-                    ResetOffset();
-                }
+
+                weaponForward = transform.forward;
+                return;
+            }
+            else if (SteamVR_Actions.valheim_Grab.GetStateUp(SteamVR_Input_Sources.LeftHand) || SteamVR_Actions.valheim_Grab.GetStateUp(SteamVR_Input_Sources.RightHand))
+            {
+                _isTwoHanded = isTwoHanded.SingleHanded;
+                ResetOffset();
             }
         }
         private bool isSpear()
