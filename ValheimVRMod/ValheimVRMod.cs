b0f2928e8cda9c89486c725b01df53966fc12c90
Brandon Mousseau <brandon.a.mousseau@gmail.com>
2022-10-01 18:41:52 -0700

    Increment version to 0.9.4

--- ValheimVRMod/ValheimVRMod.cs
+++ ValheimVRMod/ValheimVRMod.cs
@@ -10,7 +10,7 @@ using static ValheimVRMod.Utilities.LogUtils;
 
 namespace ValheimVRMod
 {
-    [BepInPlugin("org.bepinex.plugins.valheimvrmod", "ValheimVR Mod", "0.9.3")]
+    [BepInPlugin("org.bepinex.plugins.valheimvrmod", "ValheimVR Mod", "0.9.4")]
     public class ValheimVRMod : BaseUnityPlugin
     {
 
