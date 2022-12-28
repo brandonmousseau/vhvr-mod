037ee13f2685d25b20b60244f805211c0c2fa330
hhliow <hxliow@gmail.com>
2022-12-28 18:24:01 -0500

    Readability cleanup: put all hardcoded bow anatomy data in one place.

---
+++ ValheimVRMod/Utilities/BowAnatomy.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading;
+using UnityEngine;
+
+/**
+ * Manages hardcoded bow anatomy data.
+ */
+namespace ValheimVRMod.Utilities
+{
+  public class BowAnatomy
+  {
+    public readonly Vector3 stringTop;
+    public readonly Vector3 stringBottom;
+    public readonly Vector3 handleTop;
+    public readonly Vector3 handleBottom;
+    public readonly float handleWidth;
+    public readonly float handleHeight;
+    public readonly float softLimbHeight;
+    public readonly float stringRadius;
+    
+    private static BowAnatomy DefaultBowAnatomy = new BowAnatomy(New Vector3(0, 0.75f, 0.325f), New Vector3(0, -0.75f, 0.325f), New Vector3(0, 0.312f, 0), New Vector3(0, -0.312f, 0), 0.05f, 0.624f, 0.125f, 0.01f);
+    private Dictionary<string, BowAnatomy> BowAnatomies = new Dictionary<string, BowAnatomy>
+    {
+        {"$item_bow_spinesnap",, new BowAnatomy(New Vector3(0, 0.75f, 0.325f), New Vector3(0, -0.75f, 0.325f), New Vector3(0, 0.312f, 0), New Vector3(0, -0.312f, 0), 0.05f, 0.624f, 0.125f, 0.01f)}
+    }
+           
+    protected BowAnatomy(Vector3 stringTop, Vector3 stringBottom, Vector3 handleTop, Vector3 handleBottom, float handleWidth, float handleHeight, float softLimbHeight, float stringRadius) {
+      this.stringTop = stringTop;
+      this.stringBottom = stringBottom;
+      this.handleTop = handleTop;
+      this.handleBottom = handleBottom;
+      this.handleWidth = handleWidth;
+      this.handleHeight = handleHeight;
+      this.softLimbHeight = softLimbHeight;
+      this.stringRadius = stringRadius;
+    }
+    
+    public static BowAnatomy getBowAnatomy(string bowName) {
+      if (BowAnatomies.ContainsKey(bowName)) {
+        return BowAnatomies[bowName];
+      } else {
+        return DefaultBowAnatomy;
+      }
+    }
+  }
+}
