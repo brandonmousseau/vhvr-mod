38e0a2e6399c3efec4851b9b34cfc9f507808a8d
hhliow <hxliow@gmail.com>
2022-12-31 17:11:24 -0500

    Fix lingering red outline after blocking

--- ValheimVRMod/Scripts/MeshCooldown.cs
+++ ValheimVRMod/Scripts/MeshCooldown.cs
@@ -1,21 +1,28 @@
 using UnityEngine;
+using ValheimVRMod.Utilities;
 
 namespace ValheimVRMod.Scripts {
     public class MeshCooldown : MonoBehaviour {
+        public string blockType;
+
+        private static readonly Color FullOutlineColor = Color.red;
+        private static readonly Color HiddenOutlineColor = new Color(1, 0, 0, 0);
+
+        private float cooldownStart;
         private float cooldown;
         private Outline outline;
 
         public static MeshCooldown sharedInstance;
         public static bool staminaDrained;
         public static float damageMultiplier;
-        
+
         public bool tryTrigger(float cd) {
             if (inCoolDown()) {
                 return false;
             }
 
-            getOutline();
-            cooldown = cd;
+            cooldown = cooldownStart = cd;
+            resetOutline();
             if (sharedInstance == null) {
                 sharedInstance = this;
                 damageMultiplier = 1;
@@ -36,37 +43,38 @@ namespace ValheimVRMod.Scripts {
             return dmgMultiplier;
         }
 
-        private void OnDisable() {
-            Destroy(this);
+        void Awake() {
+            outline = gameObject.AddComponent<Outline>();
+        }
+
+        void OnDisable() {
+            outline.OutlineMode = Outline.Mode.OutlineHidden;
+        }
+
+        void OnDestory() {
+            Destroy(outline);
         }
 
         public bool inCoolDown() {
             return cooldown > 0;
         }
 
-        private Outline getOutline() {
-            
-            if (outline != null) {
-                return outline;
-            }
-
-            outline = gameObject.AddComponent<Outline>();
-            outline.OutlineColor = Color.red;
+        private void resetOutline() {
             outline.OutlineWidth = 10;
+            outline.OutlineColor = FullOutlineColor;
             outline.OutlineMode = Outline.Mode.OutlineVisible;
-            
-            return outline;
         }
         
         private void FixedUpdate() {
+            outline.OutlineColor = Color.Lerp(HiddenOutlineColor, FullOutlineColor, Mathf.Max(cooldown, 0) / cooldownStart);
             if (! inCoolDown()) {
                 return;
             }
 
             cooldown -= Time.fixedDeltaTime;
-            
+           
             if (! inCoolDown()) {
-                Destroy(getOutline());
+                outline.OutlineMode = Outline.Mode.OutlineHidden;
                 
                 if (sharedInstance == this) {
                     staminaDrained = false;
