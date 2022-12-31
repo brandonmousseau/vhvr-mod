887aa4973f1d078dbd32da248fb37e0dfd739fb3
hhliow <hxliow@gmail.com>
2022-12-31 17:36:09 -0500

    Readability cleanup: rename variables in BowAnatomy to be semantically more accurate

--- ValheimVRMod/Utilities/BowAnatomy.cs
+++ ValheimVRMod/Utilities/BowAnatomy.cs
@@ -16,41 +16,44 @@ namespace ValheimVRMod.Utilities
             Auto // Use skinned mesh renderer for bow bending animation if the mesh can be accessed or use custom shader otherwise
         }
 
-        public readonly Vector3 stringTop;
-        public readonly Vector3 stringBottom;
-        public readonly Vector3 handleTop;
-        public readonly Vector3 handleBottom;
-        public readonly float handleWidth;
         public readonly float handleHeight;
         public readonly float softLimbHeight;
         public readonly float stringRadius;
         public readonly BowBendingImplType bowBendingImpl;
 
+        // The following data stand when they cannot be calculated from the mesh data.
+        public readonly float fallbackHandleWidth;
+        public readonly Vector3 fallbackStringTop;
+        public readonly Vector3 fallbackStringBottom;
+        public readonly Vector3 fallbackHandleTop;
+        public readonly Vector3 fallbackHandleBottom;
+
+
         private static BowAnatomy DefaultBowAnatomy = new BowAnatomy(
-            /* stringTop= */ new Vector3(0, 0.75f, -0.325f),
-            /* stringBottom= */ new Vector3(0, -0.75f, -0.325f),
-            /* handleTop= */ new Vector3(0, 0.312f, 0),
-            /* handleBottom= */ new Vector3(0, -0.312f, 0),
-            /* handleWidth= */ 0.05f,
             /* handleHeight= */ 0.624f,
             /* softLimbHeight= */ 0.125f,
             /* stringRadius= */ 0.008f,
-            /* bowBendingImpl= */ BowBendingImplType.Skinned);
+            /* bowBendingImpl= */ BowBendingImplType.Skinned,
+            /* fallbackHandleWidth= */ 0.05f,
+            /* fallbackStringTop= */ new Vector3(0, 0.75f, -0.325f),
+            /* fallbackStringBottom= */ new Vector3(0, -0.75f, -0.325f),
+            /* fallbackHandleTop= */ new Vector3(0, 0.312f, 0),
+            /* fallbackHandleBottom= */ new Vector3(0, -0.312f, 0));
 
         private static Dictionary<string, BowAnatomy> BowAnatomies = new Dictionary<string, BowAnatomy>
         {
             {
                 "$item_bow_snipesnap", // Note: item name is snipesnap not spinesnap
                 new BowAnatomy(
-                    /* stringTop= */ new Vector3(0, 0.75f, -0.325f),
-                    /* stringBottom= */ new Vector3(0, -0.75f, -0.325f),
-                    /* handleTop= */ new Vector3(0, 0.26f, 0),
-                    /* handleBottom= */ new Vector3(0, -0.26f, 0),
-                    /* handleWidth= */ 0.05f,
                     /* handleHeight= */ 0.52f,
                     /* softLimbHeight= */ 0.15f,
                     /* stringRadius= */ 0.008f,
-                    /* bowBendingImpl= */ BowBendingImplType.Auto)
+                    /* bowBendingImpl= */ BowBendingImplType.Auto,
+                    /* fallbackHandleWidth= */ 0.05f,
+                    /* fallbackStringTop= */ new Vector3(0, 0.75f, -0.325f),
+                    /* fallbackStringBottom= */ new Vector3(0, -0.75f, -0.325f),
+                    /* fallbackHandleTop= */ new Vector3(0, 0.26f, 0),
+                    /* fallbackHandleBottom= */ new Vector3(0, -0.26f, 0))
             }
         };
 
@@ -67,25 +70,25 @@ namespace ValheimVRMod.Utilities
         }
 
         protected BowAnatomy(
-            Vector3 stringTop,
-            Vector3 stringBottom,
-            Vector3 handleTop,
-            Vector3 handleBottom,
-            float handleWidth,
             float handleHeight,
             float softLimbHeight,
             float stringRadius,
-            BowBendingImplType bowBendingImpl)
+            BowBendingImplType bowBendingImpl,
+            float fallbackHandleWidth,
+            Vector3 fallbackStringTop,
+            Vector3 fallbackStringBottom,
+            Vector3 fallbackHandleTop,
+            Vector3 fallbackHandleBottom)
         {
-            this.stringTop = stringTop;
-            this.stringBottom = stringBottom;
-            this.handleTop = handleTop;
-            this.handleBottom = handleBottom;
-            this.handleWidth = handleWidth;
             this.handleHeight = handleHeight;
             this.softLimbHeight = softLimbHeight;
             this.stringRadius = stringRadius;
             this.bowBendingImpl = bowBendingImpl;
+            this.fallbackHandleWidth = fallbackHandleWidth;
+            this.fallbackStringTop = fallbackStringTop;
+            this.fallbackStringBottom = fallbackStringBottom;
+            this.fallbackHandleTop = fallbackHandleTop;
+            this.fallbackHandleBottom = fallbackHandleBottom;
         }
     }
 }
