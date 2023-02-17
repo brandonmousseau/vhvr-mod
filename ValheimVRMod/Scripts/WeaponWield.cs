02409bf61c8e9fb8a7251776416f6538070b7c74
hhliow <hxliow@gmail.com>
2023-02-17 17:17:54 -0500

    Create base class WeaponWield

---
+++ ValheimVRMod/Scripts/WeaponWield.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using ValheimVRMod.Scripts.Block;
+using ValheimVRMod.Utilities;
+using ValheimVRMod.VRCore;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+namespace ValheimVRMod.Scripts
+{
+    // TODO: have LocalWeaponWield extend this class.
+    public class WeaponWield : MonoBehaviour
+    {
+        // TODO: move non-local-player logic from LocalWeaponWield to this class.
+    }
+}
