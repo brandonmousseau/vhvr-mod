97375cc6ab9cd191c85d8d1e18dcd183fff33dbd
hhliow <hxliow@gmail.com>
2023-03-15 22:26:56 -0400

    Update TextInputPatches to use TextMeshPro

--- ValheimVRMod/Patches/TextInputPatches.cs
+++ ValheimVRMod/Patches/TextInputPatches.cs
@@ -6,6 +6,7 @@ using UnityEngine.Events;
 using UnityEngine.UI;
 using ValheimVRMod.Utilities;
 using Valve.VR;
+using TMPro;
 
 namespace ValheimVRMod.Patches {
 
@@ -19,40 +20,72 @@ namespace ValheimVRMod.Patches {
                 instance = __instance;
                 if (VHVRConfig.AutoOpenKeyboardOnInteract() || instance.m_topic.text == "ChatText")
                 {
-                    InputManager.start(instance.m_textField, true, OnClose);
+                    InputManager.start(instance.m_textField, instance.m_textFieldTMP, true, OnClose);
                 }
             }
         }
 
         private static void OnClose() {
-            instance.Hide();
+            instance.OnEnter();
         }
     }
     
     [HarmonyPatch(typeof(InputField), "OnPointerClick")]
-    class PatchInputFieldClick {
-        public static void Postfix(InputField __instance) {
+    class PatchInputFieldClick
+    {
+        public static void Postfix(InputField __instance)
+        {
+            if (VHVRConfig.UseVrControls())
+            {
+                InputManager.start(__instance, null);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(TMP_InputField), "OnPointerClick")]
+    class PatchInputFieldTmpClick {
+        public static void Postfix(TMP_InputField __instance) {
             if (VHVRConfig.UseVrControls()) {
-                InputManager.start(__instance);
+                InputManager.start(null, __instance);
             }
         }
     }
 
     [HarmonyPatch(typeof(InputField), "OnFocus")]
-    class PatchPasswordFieldFocus {
-        public static void Postfix(InputField __instance) {
-            if(VHVRConfig.UseVrControls() && __instance.inputType == InputField.InputType.Password) {
-                InputManager.start(__instance, true);
+    class PatchPasswordFieldFocus
+    {
+        public static void Postfix(InputField __instance)
+        {
+            if (VHVRConfig.UseVrControls() && __instance.inputType == InputField.InputType.Password)
+            {
+                InputManager.start(__instance, null, true);
             }
         }
     }
     
     [HarmonyPatch(typeof(Minimap), "ShowPinNameInput")]
     class PatchMinimap {
-        public static void Postfix(InputField ___m_nameInput) {
+        private static Minimap instance;
+        public static bool pendingInputEnter { get; private set; }
+
+        public static void Postfix(Minimap __instance) {
             if (VHVRConfig.UseVrControls()) {
-                InputManager.start(___m_nameInput, true);
+                instance = __instance;
+                InputManager.start(__instance.m_nameInput, null, true, OnClose);
             }
+        }
+
+        public static void maybeClearPendingInputEnter()
+        {
+            if (instance.m_namePin == null)
+            {
+                pendingInputEnter = false;
+            }
+        }
+
+        private static void OnClose()
+        {
+            pendingInputEnter = true;
         }
     }
     
@@ -82,52 +115,70 @@ namespace ValheimVRMod.Patches {
 
         private static bool initialized;
         private static InputField _inputField;
+        private static TMP_InputField _inputFieldTmp;
         private static UnityAction _closedAction;
         private static bool _returnOnClose;
         
         public static float closeTime;
         public static bool triggerReturn;
 
-        public static void start(InputField inputField, bool returnOnClose = false, UnityAction closedAction = null) {
+        public static void start(InputField inputField, TMP_InputField inputFieldTmp, bool returnOnClose = false, UnityAction closedAction = null) {
 
             _inputField = inputField;
+            _inputFieldTmp = inputFieldTmp;
             _returnOnClose = returnOnClose;
             _closedAction = closedAction;
             
-            if (_inputField.text == "...") {
+            if (_inputField != null && _inputField.text == "...") {
                 _inputField.text = "";
             }
-            
+
+            if (_inputFieldTmp != null && _inputFieldTmp.text == "...") {
+                _inputFieldTmp.text = "";
+            }
+
             if (!initialized) {
                 SteamVR_Events.System(EVREventType.VREvent_KeyboardClosed).Listen(OnKeyboardClosed);
                 initialized = true;
             }
             
-            SteamVR.instance.overlay.ShowKeyboard(0, 0, 0, "TextInput", 256, _inputField.text, 1);
+            SteamVR.instance.overlay.ShowKeyboard(0, 0, 0, "TextInput", 256, _inputField != null ? _inputField.text : _inputFieldTmp.text, 1);
         }
 
         private static void OnKeyboardClosed(VREvent_t args) {
             closeTime = Time.fixedTime;
-            StringBuilder text = new StringBuilder(256);
-            _inputField.caretPosition = (int) SteamVR.instance.overlay.GetKeyboardText(text, 256);
-            _inputField.text = text.ToString();
+            StringBuilder textBuilder = new StringBuilder(256);
+            int caretPosition = (int)SteamVR.instance.overlay.GetKeyboardText(textBuilder, 256);
+            string text = textBuilder.ToString();
+
+            if (_inputField)
+            {
+                _inputField.caretPosition = caretPosition;
+                _inputField.text = text;
+            }
+
+            if (_inputFieldTmp )
+            {
+                _inputFieldTmp.caretPosition = caretPosition;
+                _inputFieldTmp.text = text;
+            }
 
             if (Scripts.QuickAbstract.shouldStartChat)
             {
-                if (_inputField.text != "")
+                if (text != "")
                 {
-                    if (_inputField.text.StartsWith("/cmd")) //SEND CONSOLE INPUT
+                    if (text.StartsWith("/cmd")) //SEND CONSOLE INPUT
                     {
-                        if (_inputField.text.StartsWith("/cmd "))
-                            _inputField.text = _inputField.text.Remove(0, 5);
+                        if (text.StartsWith("/cmd "))
+                            text = text.Remove(0, 5);
                         else
-                            _inputField.text = _inputField.text.Remove(0, 4);
+                            text = text.Remove(0, 4);
 
-                        Console.instance.TryRunCommand(_inputField.text);
+                        Console.instance.TryRunCommand(text);
                     }
                     else //SEND CHAT INPUT
                     {
-                        Chat.instance.m_input.text = _inputField.text;
+                        Chat.instance.m_input.text = text;
                         Chat.instance.InputText();
                         Chat.instance.m_input.text = "";
                     }
