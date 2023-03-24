cac402624c45e94f6fc808a036b7a2029213b95e
hhliow <hxliow@gmail.com>
2023-03-23 20:56:57 -0400

    Add methods for emulating key down and button down

--- ValheimVRMod/Patches/ControlPatches.cs
+++ ValheimVRMod/Patches/ControlPatches.cs
@@ -15,7 +15,19 @@ namespace ValheimVRMod.Patches {
 
     [HarmonyPatch(typeof(ZInput), nameof(ZInput.GetButtonDown))]
     class ZInput_GetButtonDown_Patch {
+        private static HashSet<string> pendingButtons = new HashSet<string>();
+
+        static public void EmulateButtonDown(string name)
+        {
+            pendingButtons.Add(name);
+        }
+
         static bool Prefix(string name, ref bool __result) {
+            if (pendingButtons.Remove(name)) {
+                __result = true;
+                return false;
+            }
+
             // Need to bypass original function for any required ZInputs that begin
             // with "Joy" to ensure the VR Controls still work when
             // Gamepad is disabled.
@@ -56,6 +68,27 @@ namespace ValheimVRMod.Patches {
             if (VRControls.mainControlsActive) {
                 __result = __result || VRControls.instance.GetButton(name);
             }
+        }
+    }
+
+    [HarmonyPatch(typeof(ZInput), nameof(ZInput.GetKeyDown))]
+    class ZInput_GetKeyDown_Patch
+    {
+        private static HashSet<KeyCode> pendingKeys = new HashSet<KeyCode>();
+
+        static public void EmulateKeyDown(KeyCode key)
+        {
+            pendingKeys.Add(key);
+        }
+
+        static bool Prefix(KeyCode key, ref bool __result)
+        {
+            if (pendingKeys.Remove(key))
+            {
+                __result = true;
+                return false;
+            }
+            return true;
         }
     }
 
