1ac6e014a1cd2ea04feea1680fbaf355134a5aca
hhliow <hxliow@gmail.com>
2023-08-23 18:29:50 -0400

    Fix broken tooltips in keyboard+mouse mode
    Enable the patch that uses SoftwareCursor.ScaledMouseVector() instead of the vanilla Input.mousePosition for keyboard+mouse mode.

--- ValheimVRMod/Patches/ControlPatches.cs
+++ ValheimVRMod/Patches/ControlPatches.cs
@@ -726,10 +726,6 @@ namespace ValheimVRMod.Patches {
     {
         static bool Prefix(InventoryGrid __instance, ref InventoryGrid.Element __result)
         {
-            if (!VHVRConfig.UseVrControls()) {
-                return true;
-            }
-
             foreach (InventoryGrid.Element element in __instance.m_elements)
             {
                 RectTransform rectTransform = element.m_go.transform as RectTransform;
