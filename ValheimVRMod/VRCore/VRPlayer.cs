2068080cc789b3d69043e4f728124dc8793c0cda
hhliow <hxliow@gmail.com>
2023-12-02 18:56:27 -0500

    Revert wrong commit

--- ValheimVRMod/VRCore/VRPlayer.cs
+++ ValheimVRMod/VRCore/VRPlayer.cs
@@ -87,7 +87,6 @@ namespace ValheimVRMod.VRCore
         public static float roomscaleAnimationForwardSpeed { get { return _roomscaleAnimationForwardSpeed; } }
         public static float roomscaleAnimationSideSpeed { get { return _roomscaleAnimationSideSpeed; } }
         public static Vector3 roomscaleMovement { get; private set; }
-        public static GesturedLocomotionManager gesturedLocomotionManager { get; private set; } = null;
 
         private static Hand _leftHand;
         private static SteamVR_LaserPointer _leftPointer;
@@ -298,7 +297,6 @@ namespace ValheimVRMod.VRCore
                 if (inFirstPerson)
                 {
                     DoRoomScaleMovement();
-                    gesturedLocomotionManager?.UpdateMovementFromGestures(Time.deltaTime);
                 }
                 else
                 {
@@ -581,7 +579,6 @@ namespace ValheimVRMod.VRCore
             vrCam.enabled = true;
             _vrCam = vrCam;
             _vrCameraRig = vrCam.transform.parent;
-            gesturedLocomotionManager = new GesturedLocomotionManager(_vrCameraRig);
 
             _fadeManager.OnFadeToWorld += () => {
                 //Recenter
