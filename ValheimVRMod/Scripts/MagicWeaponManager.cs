fec82bf369e53e29c8d0c5ac335e27bc96b26e62
hhliow <hxliow@gmail.com>
2024-05-07 06:52:53 -0400

    Add UpwardHandSummonManager

--- ValheimVRMod/Scripts/MagicWeaponManager.cs
+++ ValheimVRMod/Scripts/MagicWeaponManager.cs
@@ -11,7 +11,12 @@ namespace ValheimVRMod.Scripts {
     // TODO: implement this class as a component added to the weapon object and reduce static instance and singletion usage.
     class MagicWeaponManager {
 
-        private static readonly HashSet<string> SWING_LAUNCH_MAGIC_STAFF_NAMES = new HashSet<string>(new string[] { "$item_stafffireball" });
+        private static readonly HashSet<string> SWING_LAUNCH_MAGIC_STAFF_NAMES =
+            new HashSet<string>(new string[] {
+                "$item_stafffireball", "$item_staffgreenroots", "$item_staffclusterbomb", "$item_staffredtroll" });
+        private static readonly HashSet<string> OPPOSITE_HAND_SUMMONER_NAMES =
+            new HashSet<string>(new string[] {"$item_staffskeleton" });
+
         private static bool IsMagicWeaponEquipped { get { return EquipScript.getLeft() == EquipType.Magic || EquipScript.getRight() == EquipType.Magic;  } }
         // Right-handed weapons in vanilla game is treated as domininant hand weapon in VHVR.
         private static bool IsDominantHandWeapon { get { return EquipScript.getRight() == EquipType.Magic; } }       
@@ -23,12 +28,22 @@ namespace ValheimVRMod.Scripts {
         public static Vector3 AimDir {
             get
             {
+                if (CanSummonWithOppositeHand())
+                {
+                    return VHVRConfig.LeftHanded() ? VRPlayer.leftHandBone.up : VRPlayer.rightHandBone.up;
+                }
                 return UseSwingForCurrentAttack() ? SwingLaunchManager.aimDir : LocalWeaponWield.isCurrentlyTwoHanded() ? LocalWeaponWield.weaponForward : WeaponHandPointer.rayDirection * Vector3.forward;
             }
         }
 
         public static bool IsSwingLaunchEnabled() {
             return SWING_LAUNCH_MAGIC_STAFF_NAMES.Contains(Player.m_localPlayer?.GetRightItem()?.m_shared?.m_name); 
+        }
+
+        public static bool CanSummonWithOppositeHand()
+        {
+            return IsMagicWeaponEquipped && OPPOSITE_HAND_SUMMONER_NAMES.Contains(Player.m_localPlayer?.GetLeftItem()?.m_shared?.m_name);
+
         }
 
         public static bool AttemptingAttack {
@@ -38,8 +53,21 @@ namespace ValheimVRMod.Scripts {
                 {
                     return false;
                 }
+
+                if (CanSummonWithOppositeHand() && UpwardHandSummonManager.pendingSummon)
+                {
+                    UpwardHandSummonManager.pendingSummon = false;
+                    return true;
+                }
+
                 return UseSwingForCurrentAttack() ? SwingLaunchManager.isThrowing : AttackTriggerAction.state;
             }
+        }
+
+        public static bool ShouldUseVanillaMagicAnimation(Player player)
+        {
+            var attack = player.m_currentAttack;
+            return player.InAttack() && attack != null && attack.m_attackAnimation.Contains("staff_summon");
         }
 
         public static bool ShouldSkipAttackAnimation()
@@ -49,7 +77,10 @@ namespace ValheimVRMod.Scripts {
 
         public static Vector3 GetProjectileSpawnPoint(Attack attack) 
         {
-            var offsetDirection = LocalWeaponWield.weaponForward;
+            var offsetDirection =
+                CanSummonWithOppositeHand() ?
+                (VHVRConfig.LeftHanded() ? VRPlayer.leftHandBone.up : VRPlayer.rightHandBone.up) :
+                LocalWeaponWield.weaponForward;
             var offsetAmount =
                 (new Vector3(attack.m_attackOffset, attack.m_attackRange, attack.m_attackHeight)).magnitude;
             if (attack.m_attackAnimation.Contains("summon"))
@@ -70,6 +101,57 @@ namespace ValheimVRMod.Scripts {
             // Disable swing launch if the staff is held with two hands like a rifle
             // (dominant hand behind the other hand).
             return IsSwingLaunchEnabled() && !LocalWeaponWield.IsDominantHandBehind;
+        }
+
+        public class UpwardHandSummonManager : MonoBehaviour
+        {
+            private const float MIN_SUMMONING_HAND_SPEED = 0.25f;
+            private const float SUMMON_TIME = 1.5f;
+            private float currentMaxHandHight = Mathf.NegativeInfinity;
+            private float summonTimer = 0;
+            private bool hasSummonedInCurrentMotion = false;
+
+            public static bool pendingSummon = false;
+
+            void FixedUpdate()
+            {
+                if (SteamVR_Actions.valheim_Use.GetState(VRPlayer.dominantHandInputSource))
+                {
+                    if (hasSummonedInCurrentMotion)
+                    {
+                        return;
+                    }
+
+                    float handHeight = VRPlayer.dominantHand.transform.position.y;
+                    if (handHeight < currentMaxHandHight)
+                    {
+                        // Pause summoning unless the hand is moving upward.
+                        return;
+                    }
+                    currentMaxHandHight = handHeight;
+
+                    var velocity =
+                        (VHVRConfig.LeftHanded() ? VRPlayer.leftHandPhysicsEstimator : VRPlayer.rightHandPhysicsEstimator).GetVelocity();
+                    if (velocity.y > MIN_SUMMONING_HAND_SPEED)
+                    {
+                        summonTimer += Time.fixedDeltaTime;
+                        VRPlayer.dominantHand.hapticAction.Execute(
+                            0, 0.1f, 50, 0.3f, VRPlayer.dominantHandInputSource);
+                    }
+
+                    if (summonTimer > SUMMON_TIME)
+                    {
+                        hasSummonedInCurrentMotion = true;
+                        pendingSummon = true;
+                    }
+                }
+                else
+                {
+                    pendingSummon = hasSummonedInCurrentMotion = false;
+                    summonTimer = 0;
+                    currentMaxHandHight = float.NegativeInfinity;
+                }
+            }
         }
     }
  }
