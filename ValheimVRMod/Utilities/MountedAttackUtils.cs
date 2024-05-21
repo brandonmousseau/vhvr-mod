b3d983a7e8afe01f73003b5576ecfd251bce4676
hhliow <hxliow@gmail.com>
2024-05-21 19:05:13 -0400

    Keep player rotation when using mounted attack

--- ValheimVRMod/Utilities/MountedAttackUtils.cs
+++ ValheimVRMod/Utilities/MountedAttackUtils.cs
@@ -71,8 +71,9 @@ namespace ValheimVRMod.Utilities
 
             Attack attack =
                 isSecondaryAttack ? weapon.m_shared.m_secondaryAttack.Clone() : weapon.m_shared.m_attack.Clone();
-            // Player.m_localPlayer.m_attack = true;
-            // Player.m_localPlayer.m_attackHold = true;
+            var playerRotation = player.transform.rotation;
+            var bodyRotation = player.m_body.rotation;
+
             // The rest is cloned from vanilla game logic.
             if (attack.Start(
                 player,
@@ -90,6 +91,9 @@ namespace ValheimVRMod.Utilities
                 player.m_currentAttack = attack;
                 player.m_currentAttackIsSecondary = false;
                 player.m_lastCombatTimer = 0f;
+                // Restore the rotation since vanilla attack logic may have changed it.
+                player.transform.rotation = playerRotation;
+                player.m_body.rotation = bodyRotation;
                 return true;
             }
 
