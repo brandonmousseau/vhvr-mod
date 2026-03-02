131a44b218d218a428b27f3ba7ad2f7fcd1acf74
hhliow <hxliow@gmail.com>
2026-03-02 18:12:10 -0500

    Implement red-out to warn low health

--- ValheimVRMod/Scripts/FadeToBlackManager.cs
+++ ValheimVRMod/Scripts/FadeToBlackManager.cs
@@ -1,8 +1,7 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using Valve.VR;
-using UnityEngine.PostProcessing;
-using UnityEngine.Rendering;
 
 namespace ValheimVRMod.Scripts 
 {
@@ -15,8 +14,8 @@ namespace ValheimVRMod.Scripts
         public event Action OnFadeToBlack;
         public event Action OnFadeToWorld;
 
-        private bool _lastShouldFade = false;
-        public bool IsFadingToBlack => _lastShouldFade;
+        private bool _lastShouldFadeToBlack = false;
+        public bool IsFadingToBlack => _lastShouldFadeToBlack;
 
         private bool ShouldFadeToBlack => (Player.m_localPlayer != null && (
                                         Player.m_localPlayer.IsSleeping() 
@@ -25,23 +24,93 @@ namespace ValheimVRMod.Scripts
                                         || Player.m_localPlayer.IsTeleporting())) ||
                                         (Hud.instance?.m_loadingScreen && Hud.instance.m_loadingScreen.isActiveAndEnabled);
 
+        private Coroutine lowHealthPulseCoroutine;
+        private bool isLowHealthPulsing;
+        private float lowHealthPulseAlpha;
+        private float lowHealthPulseInterval;
 
         private void FixedUpdate()
         {
-            bool shouldFade = ShouldFadeToBlack;
-            if(_lastShouldFade != shouldFade)
+            if (ShouldFadeToBlack)
             {
-                if(shouldFade)
+                StopLowHealthPulse();
+                if (!_lastShouldFadeToBlack)
                 {
                     SteamVR_Fade.Start(Color.black, 0.2f);
                     OnFadeToBlack?.Invoke();
+                    _lastShouldFadeToBlack = true;
                 }
-                else
+            }
+            else
+            {
+                if (_lastShouldFadeToBlack)
                 {
                     SteamVR_Fade.Start(Color.clear, 0.15f);
                     OnFadeToWorld?.Invoke();
+                    _lastShouldFadeToBlack = false;
                 }
-                _lastShouldFade = shouldFade;
+                UpdateLowHealthPulse();
+            }
+        }
+
+        private void UpdateLowHealthPulse() {
+            var player = Player.m_localPlayer;
+            if (player == null)
+            {
+                StopLowHealthPulse();
+                return;
+            }
+
+            var maxHealth = player.GetMaxHealth();
+            var currentHealth = player.GetHealth();
+            var warningHealth = Mathf.Min(maxHealth * 0.25f, 32);
+
+            if (currentHealth > warningHealth) {
+                StopLowHealthPulse();
+                return;
+            }
+
+            lowHealthPulseAlpha = Mathf.Lerp(0.25f, 0, currentHealth / warningHealth);
+            lowHealthPulseInterval = Mathf.Min(currentHealth, 32) / 64 + 0.25f;
+            StartLowHealthPulse();
+        }
+
+        private void StartLowHealthPulse()
+        {
+            if (isLowHealthPulsing)
+            {
+                return;
+            }
+            if (lowHealthPulseCoroutine != null)
+            {
+                StopCoroutine(lowHealthPulseCoroutine);
+            }
+            lowHealthPulseCoroutine = StartCoroutine(PulseRed());
+            isLowHealthPulsing = true;
+        }
+
+        private void StopLowHealthPulse()
+        {
+            if (!isLowHealthPulsing)
+            {
+                return;
+            }
+            if (lowHealthPulseCoroutine != null)
+            {
+                StopCoroutine(lowHealthPulseCoroutine);
+            }
+            SteamVR_Fade.Start(Color.clear, 0.5f);
+            isLowHealthPulsing = false;
+        }
+
+        private IEnumerator PulseRed()
+        {
+            while (true)
+            {
+                SteamVR_Fade.Start(new Color(1f, 0f, 0f, lowHealthPulseAlpha), lowHealthPulseInterval);
+                yield return new WaitForSeconds(lowHealthPulseInterval);
+                SteamVR_Fade.Start(new Color(1f, 0f, 0f, 0f), lowHealthPulseInterval);
+                yield return new WaitForSeconds(lowHealthPulseInterval);
             }
         }
     }
