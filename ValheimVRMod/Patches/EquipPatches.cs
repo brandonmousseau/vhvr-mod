aa7878be9a512036a66fecf5aa04ac3149268896
hhliow <hxliow@gmail.com>
2026-03-28 20:44:01 -0400

    Fix the bug that other players' weapon appear on local player's hand

--- ValheimVRMod/Patches/EquipPatches.cs
+++ ValheimVRMod/Patches/EquipPatches.cs
@@ -252,6 +252,7 @@ namespace ValheimVRMod.Patches {
                             return;
                     }
                 }
+                return;
             }
 
             if (VHVRConfig.NonVrPlayer())
