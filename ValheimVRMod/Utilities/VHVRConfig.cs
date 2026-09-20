cd9d70e0ce1aab0e685560313c7c42b90a93d98a
Aceship <terrenceleroy@yahoo.com>
2026-09-20 14:26:08 +0700

    reverted the config order for later

--- ValheimVRMod/Utilities/VHVRConfig.cs
+++ ValheimVRMod/Utilities/VHVRConfig.cs
@@ -166,14 +166,13 @@ namespace ValheimVRMod.Utilities
         private static ConfigEntry<string> useSpearDirectionGraphic;
         private static ConfigEntry<float> fullThrowSpeed;
         private static ConfigEntry<bool> spearInverseWield;
-        private static ConfigEntry<string> meleeAttackKnockbackDirection;
         private static ConfigEntry<string> twoHandedWield;
-        private static ConfigEntry<bool> movementSecondaryAttack;
         private static ConfigEntry<bool> twoHandedWithShield;
         private static ConfigEntry<string> blockingType;
         private static ConfigEntry<float> shieldScale;
         private static ConfigEntry<float> maxShieldWidth;
-        
+        private static ConfigEntry<bool> movementSecondaryAttack;
+        private static ConfigEntry<string> meleeAttackKnockbackDirection;
 
 #if DEBUG
         private static ConfigEntry<float> DebugPosX;
@@ -1031,22 +1030,12 @@ namespace ValheimVRMod.Utilities
                                                     "TriggerGrip - Holding both Grip and Trigger to make the direction line appear",
                                                     new AcceptableValueList<string>(new string[] { "Grip", "TriggerGrip", "Disabled" })));
             //Two-handed Changes
-            meleeAttackKnockbackDirection = config.Bind("Motion Control",
-                                        "MeleeAttackKnockbackDirection",
-                                        "Basic",
-                                        new ConfigDescription("Melee Attack Knockback Direction: " +
-                                        "Basic - Melee knockback direction away from player position. " +
-                                        "Swing - Melee knockback follow swing direction.",
-                                        new AcceptableValueList<string>(new string[] { "Basic", "Swing" })));
             twoHandedWield = config.Bind(
                 "Motion Control", "TwoHandedWield", "PolearmSticky",
                 new ConfigDescription(
                     "Use this to toggle controls of two handed weapon (left & right hand grab on weapon), allow blocking and better weapon handling.",
                     new AcceptableValueList<string>(new string[] { "NonSticky", "PolearmSticky", "Sticky", "Disabled" })));
-            movementSecondaryAttack = config.Bind("Motion Control",
-                                                    "KnifeMovementSecondaryAttack",
-                                                    false,
-                                                    "When enabled, Weapon that have movement secondary attack (Knife) button secondary attack will have 2 step, first trigger-release will make you leap, the second one works like usual button secondary attack. Re-equip after changing setting to update");
+
             twoHandedWithShield = config.Bind("Motion Control",
                                                     "TwoHandedWithShield",
                                                     false,
@@ -1060,7 +1049,7 @@ namespace ValheimVRMod.Utilities
                                         "Grab button - Block by aiming and pressing grab button, parry by timing the grab button. " +
                                         "Realistic - Block precisely where the enemy hits, swing while blocking to parry",
                                         new AcceptableValueList<string>(new string[] { "Gesture", "GrabButton", "Realistic" })));
-            
+
             shieldScale = config.Bind("Motion Control",
                 "ShieldScale",
                 1.0f,
@@ -1073,6 +1062,19 @@ namespace ValheimVRMod.Utilities
                 new ConfigDescription("The max allowed width of a shield in meters - any shield wider will be shrinked to fit this width",
                 new AcceptableValueRange<float>(0.05f, 3.0f)));
 
+            meleeAttackKnockbackDirection = config.Bind("Motion Control",
+                                        "MeleeAttackKnockbackDirection",
+                                        "Basic",
+                                        new ConfigDescription("Melee Attack Knockback Direction: " +
+                                        "Basic - Melee knockback direction away from player position. " +
+                                        "Swing - Melee knockback follow swing direction.",
+                                        new AcceptableValueList<string>(new string[] { "Basic", "Swing" })));
+
+            movementSecondaryAttack = config.Bind("Motion Control",
+                                                    "KnifeMovementSecondaryAttack",
+                                                    false,
+                                                    "When enabled, Weapon that have movement secondary attack (Knife) button secondary attack will have 2 step, first trigger-release will make you leap, the second one works like usual button secondary attack. Re-equip after changing setting to update");
+            
             advancedBuildMode = config.Bind("Motion Control",
                                                    "AdvancedBuildMode",
                                                    false,
